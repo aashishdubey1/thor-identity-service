e04c42c7ebcde09e885496a5543e7f2ec47c2209
Aashish <aashishdubey.dev@gmail.com>
2025-07-30 08:34:40 +0530

    fix: added input alidation for login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import controller from '../controller'
 import { validate } from '../middlewares/validateUser'
-import { userSchema } from '../schemas/user'
+import { userLoginSchema, userSchema } from '../schemas/user'
 const router = express.Router()
 const {authController} = controller
 
-router.post('/login',authController.loginUser)
+router.post('/login',validate(userLoginSchema),authController.loginUser)
 router.post('/register',validate(userSchema),authController.registerUser)
 router.post('/logout',authController.logoutUser)
 
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,10 +1,16 @@
-import {z} from 'zod'
+import {email, z} from 'zod'
 
 export const userSchema = z.object({
     username:z.string().min(3),
     email:z.email(),
     password:z.string()
 })
+
+export const userLoginSchema = z.object({
+    email:z.email(),
+    password:z.string().min(8)
+})
+
 
 export type UserType = z.infer<typeof userSchema>
 
